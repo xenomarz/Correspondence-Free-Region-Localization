108d376369cb08a45631cee37cbd36327c88b684
Elias <elias.jadon1@gmail.com>
2019-11-19 22:38:32 +0200

    small change

--- libs/optimization_lib/src/solvers/solver.cpp
+++ libs/optimization_lib/src/solvers/solver.cpp
@@ -50,7 +50,7 @@ int solver::run()
 void solver::linesearch()
 {
 	double step_size;
-	if (FlipAvoidingLineSearch)
+	if (/*FlipAvoidingLineSearch*/false)
 	{
 		double min_step_to_singularity;
 		if (IsConstrObjFunc) {
